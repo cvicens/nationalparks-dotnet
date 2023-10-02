dab3740f86c084da3110bf8d75f55216461ad73c
cvicens <carlos.vicens.alonso@gmail.com>
2023-10-02 09:37:47 +0200

    local test and pipeline fixed for multi-language

---
+++ run.sh
@@ -0,0 +1,8 @@
+export MONGODB_SERVER_HOST=mongodb
+export MONGODB_DATABASE=parksapp
+export MONGODB_USER=parksapp
+export MONGODB_PASSWORD=keepsafe
+
+dotnet publish
+
+./bin/Debug/net7.0/NationalParks
